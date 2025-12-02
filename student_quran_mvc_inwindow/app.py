56e2f60bd3308e281d785639360b2ac934b72b56
OUSSAMA-MDH <oussama.meddah@univ-bouira.dz>
2025-12-02 21:14:34 +0100

    Delete student_quran_mvc_inwindow directory

--- student_quran_mvc_inwindow/app.py
+++
@@ -1,8 +0,0 @@
-from student_view import StudentView
-from student_controller import StudentController
-
-if __name__ == "__main__":
-    view = StudentView(None)
-    controller = StudentController(view)
-    view.controller = controller
-    view.mainloop()
